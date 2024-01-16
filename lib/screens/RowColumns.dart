e7d0fe16409c964b38c3999dbb3e16d3f42fa169
FaisalKhawaj <faisal.khawaj@conovoinc.com>
2024-01-16 17:22:48 +0500

    test

---
+++ lib/screens/RowColumns.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RowsColumns extends StatefulWidget {
+  const RowsColumns({super.key});
+
+  @override
+  State<RowsColumns> createState() => _RowsColumnsState();
+}
+
+class _RowsColumnsState extends State<RowsColumns> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
