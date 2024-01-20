d7e4dbc5b84a6a8f7283e27f7329bfc3b0770e8f
FaisalKhawaj <khawajfaisal981@gmail.com>
2024-01-20 13:32:30 +0500

    change

--- lib/screens/listview/ListViewPractice.dart
+++ lib/screens/listview/ListViewPractice.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ListViewPractice extends StatelessWidget {
   ListViewPractice({super.key});
 
 
 List<String>  products = ["Bed",'Sofa','Chair','Aaaa','Bedroommm','DD','EEEEE','ZZZZ'];
-
+List <String> productsList=['https://picsum.photos/200','https://picsum.photos/id/237/200/300','https://picsum.photos/200/300?grayscale','https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4','https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68','https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I'];
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +22,9 @@ List<String>  products = ["Bed",'Sofa','Chair','Aaaa','Bedroommm','DD','EEEEE','
       ),
       body: Column(
             children: [
-             Container(height: 210,
-               // color: Colors.red,
+             Container(
+               height: 210,
+               color: Colors.red,
                padding:const EdgeInsets.symmetric(horizontal: 10),
                child:  ListView.builder(
                  itemCount: products.length,
@@ -46,14 +48,14 @@ List<String>  products = ["Bed",'Sofa','Chair','Aaaa','Bedroommm','DD','EEEEE','
 
                          color: Colors.white,
                        ),
-                       // height: 210,
+
                        child: Column(
                          children: [
                            ClipRRect(
                              borderRadius: BorderRadius.only( topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0)),
                              child:  Image.network('https://picsum.photos/200/300',
                                width: MediaQuery.of(context).size.width,
-                               height: 100,
+                               height: 120,
                                fit: BoxFit.cover,
                              ),
                            ),
@@ -70,28 +72,41 @@ List<String>  products = ["Bed",'Sofa','Chair','Aaaa','Bedroommm','DD','EEEEE','
                          ],
                        )
                    );
-                 }),)
-              // ListView.builder(
-              //     itemCount: products.length,
-              //     shrinkWrap: true,
-              //     itemBuilder: (context,index){
-              //       return Column(
-              //         children: [
-              //           ListTile(
-              //             leading:CircleAvatar(child:
-              //             Text(products[index][0])
-              //               ,),
-              //             title: Text(products[index],style: TextStyle(
-              //                 fontSize: 18,
-              //                 fontFamily: 'Comfortaa',
-              //                 fontWeight: FontWeight.w600
-              //             ),),
-              //             trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete),),
-              //           ),
-              //           Divider(color: Colors.brown,thickness: 0.2,)
-              //         ],
-              //       );
-              //     })
+                 }),),
+            Container(
+              child:  ListView.builder(
+                itemCount: productsList.length,
+                shrinkWrap: true,
+                physics: ScrollPhysics(parent: null),
+                // scrollDirection: Axis.vertical,
+                itemBuilder: (context,index){
+                  return Container(
+                      width: MediaQuery.of(context).size.width-20,
+                      height: 300,
+                      margin:const EdgeInsets.symmetric( vertical: 10),
+                      decoration:const BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.red,// Color of the shadow
+                            spreadRadius: 1,  // Spread radius
+                            blurRadius: 1,  // Blur radius
+                            offset: Offset(1, 1),  // Offset in x and y directions
+                          ),
+                        ],
+                        color: Colors.orange,
+                      ),
+
+                      child: ClipRRect(child: Image.network(productsList[index],
+                        width: MediaQuery.of(context).size.width,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      )
+                  );
+                }) ,)
+
             ],
           ),
 
